413d7e2694c28856e86093f330b4be3d7794fa52
Abdul Habra <abd.habra@tdameritrade.com>
2016-09-08 18:55:07 -0400

    added CollectionTools.contains() with predicate matcher

--- src/main/java/com/tek271/util2/collection/CollectionTools.java
+++ src/main/java/com/tek271/util2/collection/CollectionTools.java
@@ -3,6 +3,7 @@ package com.tek271.util2.collection;
 import org.apache.commons.lang3.ObjectUtils;
 
 import java.util.Collection;
+import java.util.function.BiPredicate;
 
 public class CollectionTools {
 
@@ -15,6 +16,13 @@ public class CollectionTools {
 
 	public <T, C extends Collection<T>> C copy(C from) {
 		return ObjectUtils.clone(from);
+	}
+
+	public <T> boolean contains(Collection<T> col, T target, BiPredicate<T,T> matcher) {
+		for (T c: col) {
+			if (matcher.test(c, target)) return true;
+		}
+		return false;
 	}
 
 }
--- src/test/java/com/tek271/util2/collection/CollectionToolsTest.java
+++ src/test/java/com/tek271/util2/collection/CollectionToolsTest.java
@@ -8,6 +8,8 @@ import java.util.List;
 import static com.google.common.collect.Lists.newArrayList;
 import static com.google.common.collect.Sets.newHashSet;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class CollectionToolsTest {
 	private static final List<Integer> LIST = newArrayList(0, 1, 2, 3);
@@ -29,6 +31,15 @@ public class CollectionToolsTest {
 		assertEquals(null, sut.copy(null));
 		assertEquals(LIST, sut.copy(LIST));
 		assertEquals(newHashSet("1", "a"), sut.copy(newHashSet("a", "1")));
+	}
+
+	@Test
+	public void testContains() {
+		assertTrue(sut.contains(LIST, 1, Integer::equals));
+		assertFalse(sut.contains(LIST, 9, Integer::equals));
+
+		List<String> list = newArrayList("ab", "cd");
+		assertTrue(sut.contains(list, "c", String::startsWith));
 	}
 
 }
